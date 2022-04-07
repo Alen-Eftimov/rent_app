7db21e9d419e13d4369255e3b803f0ce76e65662
Alen <a.eftimov@site-works.de>
2022-04-07 11:00:16 +0200

    render_post

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
 import styles from './styles';
+
 const Post = (props) => {
     return (
         <View style={styles.container}>
@@ -11,12 +12,26 @@ const Post = (props) => {
             />
 
             {/* Bed & Bedroom */}
+            <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
 
             {/* Type & Description */}
+            <Text style={styles.description} numberOfLines={2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. 
+            </Text>
 
             {/* Old price & new price */}
+            <Text style={styles.prices}>
+                <Text style={styles.oldPrice}>$45</Text>
+                <Text style={styles.price}>  $38 </Text>
+                / night
+            </Text>
 
             {/* Total price */}
+            <Text style={styles.totalPrice}>$267 total</Text>
+
         </View>
     );
 };
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -10,6 +10,30 @@ const styles = StyleSheet.create ({
         resizeMode: 'cover',
         borderRadius: 10,
     },
+    bedrooms: {
+        marginVertical: 10,
+        color: '#9e9e9e',
+    },
+    description: {
+        fontSize: 18,
+        lineHeight: 26,
+    },
+    prices: {
+        fontSize: 18,
+        marginVertical: 10,
+    },
+    oldPrice: {
+        color: '#9e9e9e',
+        textDecorationLine: 'line-through',
+    },
+    price: {
+        fontWeight: 'bold'
+    },
+    totalPrice: {
+        color: '#9e9e9e',
+        textDecorationLine: 'underline',
+    },
+
 });
 
 export default styles;
